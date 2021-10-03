4e0c55b62b11081c3dcd2d586e8dfb5f9e9490fe
Milas <84676044+killersparrow0@users.noreply.github.com>
2021-10-03 20:34:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = os.environ.get("2018618224:AAH537eWwzoqHNtD1bJuQna_OaEKTmm0oD8")
 APP_ID = int(os.environ.get("4080786"))
 API_HASH = os.environ.get("b214aec132378022e02df812714ba7a4")
 
-youtube_next_fetch = 0  # time in minute
+youtube_next_fetch = 5  # time in minute
 
 
 EDIT_TIME = 5
